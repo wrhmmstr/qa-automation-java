d3f2c84d00e3c9131c7ad0647c7020f32189ac2b
Timofey Kharchenko <t.m.khrchenko@tinkoff.ru>
2022-03-25 10:11:49 +0500

    docs: добавил комментарии в ConsolePrinter.java
    добавил javadoc-комментарии в ConsolePrinter.java для класса и метода в рамках выполнения домашнего задания "Тренировка #3-1: Комментарии"

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,18 @@
+/**
+ * Вывод информации в консоль
+ *
+ * @author t.m.kharchenko
+ * @see #print(String) Метод для вывода информации в консоль
+ */
+
 public class ConsolePrinter {
+    /**
+     * Метод выводит переданную информацию в консоль и переходит на новую строку.
+     * Побочные эффекты пока отсутствуют.
+     *
+     * @param message Строка (переменная типа String) для вывода
+     * @see ConsolePrinter Родительский класс
+     */
     public static void print(String message) {
         System.out.println(message);
     }
