a7640cd8c55e844a42541f9ba3747af75f2cab65
Timofey Kharchenko <t.m.khrchenko@tinkoff.ru>
2022-04-17 23:01:25 +0500

    feat: добавил случайный выбор важности сообщения
    Добавил случайный выбор важности сообщения для демонстрации домашнего задания 6-1

--- src/test/java/com/tcs/edu/Application.java
+++ src/test/java/com/tcs/edu/Application.java
@@ -1,13 +1,13 @@
 package com.tcs.edu;
 
 import com.tcs.edu.decorator.MessageService;
-import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.randomizer.ConsoleOutputRandomizer;
+import com.tcs.edu.randomizer.EnumRandomizer;
 
 class Application {
     public static void main(String[] args) {
         for (int i = 0; i < ConsoleOutputRandomizer.randomize(4, 8); ++i){
-            MessageService.processMessage(Severity.MINOR,"Hello world!");
+            MessageService.processMessage(EnumRandomizer.randomize(),"Hello world!");
         }
     }
 }
---
+++ src/test/java/com/tcs/edu/randomizer/EnumRandomizer.java
@@ -0,0 +1,16 @@
+package com.tcs.edu.randomizer;
+
+import com.tcs.edu.decorator.Severity;
+import java.util.Random;
+/**
+ * Вычисление случайного случайного порядкового номера enum Severity
+ *
+ * @author  t.m.kharchenko
+ * @see     #randomize() Метод для вычисления случайного порядкового номера enum Severity
+ */
+public class EnumRandomizer {
+    public static Severity randomize() {
+        Random random = new Random();
+        return Severity.values()[random.nextInt(Severity.values().length)];
+    }
+}
