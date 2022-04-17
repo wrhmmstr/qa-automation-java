27bee694aebb78b20e8bceaf1572632fdc31a110
Timofey Kharchenko <t.m.khrchenko@tinkoff.ru>
2022-04-17 21:28:50 +0500

    feat: добавил API обработки сообщения
    Добавил API обработки сообщения, в рамках домашнего задания Тренировка #06-1: Уровни значимости сообщений с кодированием перечислением

---
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -0,0 +1,24 @@
+package com.tcs.edu.decorator;
+
+import static com.tcs.edu.decorator.PagingDecorator.messageToPage;
+import static com.tcs.edu.decorator.SeverityDecorator.mapToString;
+import static com.tcs.edu.decorator.TimestampMessageDecorator.*;
+import static com.tcs.edu.printer.ConsolePrinter.print;
+
+public class MessageService {
+    /**
+     * API преобразует декорированное сообщение, уровень важности и разделитель страницы в строку для вывода в консоль
+     * и выводит полученную строку на печать.
+     * Побочные эффекты пока отсутствуют.
+     *
+     * @param level     Перечислимый тип (переменная типа Severity) с уровнем важности сообщения
+     * @param message   Строка (переменная типа String) с сообщением для декорирования
+     * @see             MessageService Родительский класс
+     */
+    public static void processMessage (Severity level, String message) {
+        final String TimestampDecoratedMessage = decorate(message);
+        final String SeverityDecoratedString = mapToString(level);
+        final String PagingDecoratedString = messageToPage(messageCount);
+        print(String.format("%s %s %s", TimestampDecoratedMessage, SeverityDecoratedString, PagingDecoratedString));
+    }
+}
