048aab1ba64c394c4e1ede7b112f21b55b6bad7d
Timofey Kharchenko <t.m.khrchenko@tinkoff.ru>
2022-04-17 23:49:49 +0500

    feat: добавил возможность вывода нескольких сообщений
    В методе processMessage класса MessageService добавил возможность вывода нескольких сообщений через varargs, в рамках выполнения домашнего задания Тренировка #06-2: Удобный вывод группы сообщений с varargs и foreach

--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -8,7 +8,7 @@ import static com.tcs.edu.printer.ConsolePrinter.print;
  * Преобразование декорированного сообщения, уровня важности и разделителя в строку
  *
  * @author  t.m.kharchenko
- * @see     #processMessage(Severity, String)  Метод для преобразования декорированного сообщения, уровня важности и разделителя в строку
+ * @see     #processMessage(Severity, String...)  Метод для преобразования декорированного сообщения, уровня важности и разделителя в строку
  */
 public class MessageService {
     /**
@@ -17,13 +17,12 @@ public class MessageService {
      * Побочные эффекты пока отсутствуют.
      *
      * @param level     Перечислимый тип (переменная типа Severity) с уровнем важности сообщения
-     * @param message   Строка (переменная типа String) с сообщением для декорирования
+     * @param messages  Массив строк (String varargs) с сообщением для декорирования
      * @see             MessageService Родительский класс
      */
-    public static void processMessage (Severity level, String message) {
-        final String TimestampDecoratedMessage = decorate(message);
-        final String SeverityDecoratedString = mapToString(level);
-        final String PagingDecoratedString = messageToPage(messageCount);
-        print(String.format("%s %s %s", TimestampDecoratedMessage, SeverityDecoratedString, PagingDecoratedString));
+    public static void processMessage (Severity level, String... messages) {
+        for (int currentMessage = 0; currentMessage < messages.length; currentMessage++) {
+            print(String.format("%s %s %s", decorate(messages[currentMessage]), mapToString(level), messageToPage(messageCount)));
+        }
     }
 }
