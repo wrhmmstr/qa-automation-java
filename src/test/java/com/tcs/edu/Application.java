5f02e21b0ee75e4f29fefc5fc46098d50eb7e95f
Timofey Kharchenko <t.m.khrchenko@tinkoff.ru>
2022-05-10 18:23:57 +0500

    test: изменил тестовый пример
    Изменил тестовый пример для проверки домашнего задания

--- src/test/java/com/tcs/edu/Application.java
+++ src/test/java/com/tcs/edu/Application.java
@@ -13,25 +13,12 @@ class Application {
             processMessages(EnumRandomizer.randomize(), "Hello world!", "Ciao mondo!", "¡Hola Mundo!");
         }*/
 
-        //для проверки домашнего задания 7-1:
-        /*processMessages(Severity.MINOR, (String) null, "Ciao mondo!", "¡Hola Mundo!");
-        processMessages(Severity.REGULAR, MessageOrder.DESC, "Hello world! 1", null, "¡Hola Mundo! 3", "Hallo Welt! 4");*/
-
-
-        //для проверки домашнего задания 7-1 и 7-2:
-        /*processMessages(Severity.MINOR, MessageOrder.DESC, null, "Ciao mondo! 2", "¡Hola Mundo! 3", "Hallo Welt! 4");
-        processMessages(Severity.REGULAR, MessageOrder.DESC, "Hello world! 1", null, "¡Hola Mundo! 3", "Hallo Welt! 4");
-        processMessages(Severity.REGULAR, MessageOrder.DESC, "Hello world! 1", "Ciao mondo! 2", null, "Hallo Welt! 4");
-        processMessages(null, MessageOrder.DESC, "Hello world! 1", "Ciao mondo! 2", "¡Hola Mundo! 3", "Hallo Welt! 4");
-        processMessages(Severity.MINOR, MessageOrder.DESC, null, null, "¡Hola Mundo! 3", "Hallo Welt! 4");
-        processMessages(null, MessageOrder.DESC, null, null, null, "Hallo Welt! 4");*/
-
-        //для проверки домашнего задания 7-1, 7-2 и 7-3:
-        processMessages(Severity.MINOR, (String) null, "Ciao mondo!", "¡Hola Mundo!");
+        processMessages(null, "Hello world! 1", "Ciao mondo!", "¡Hola Mundo!");
+        processMessages(Severity.REGULAR, (MessageOrder) null, "Hello world! 1", null, "¡Hola Mundo! 3", "Hallo Welt! 4");
         processMessages(Severity.REGULAR, MessageOrder.DESC, "Hello world! 1", null, "¡Hola Mundo! 3", "Hallo Welt! 4");
         processMessages(Severity.REGULAR, MessageOrder.ASC, Doubling.DISTINCT, "Hello world!", null, "Hello world!", "Ciao mondo!", "¡Hola Mundo!", "Hallo Welt!");
-        processMessages(Severity.REGULAR, MessageOrder.DESC, Doubling.DISTINCT, "Hello world!", null, "Hello world!", "Ciao mondo!", "¡Hola Mundo!", "Hallo Welt!");
-        processMessages(Severity.MAJOR, MessageOrder.ASC, Doubling.DISTINCT, "Hello world!",  "Ciao mondo!", "¡Hola Mundo!", "Hallo Welt!", "Hei Verden!", "Hei maailma!");
+        processMessages(Severity.REGULAR, null, Doubling.DISTINCT, "Hello world!", null, "Hello world!", "Ciao mondo!", "¡Hola Mundo!", "Hallo Welt!");
+        processMessages(Severity.MAJOR, MessageOrder.DESC, Doubling.DISTINCT, "Hello world!", "Hello world!", "Ciao mondo!", "¡Hola Mundo!", "Hallo Welt!", "Hei Verden!", "Hei maailma!");
 
 
     }
